43486bd878b0e76ad2d535edc62496c8641d20cb
djangouser <wojciech.falba@gmail.com>
2020-07-28 15:06:18 +0200

    Pocztek nauki html

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('html_tut/', views.html_tut, name='html_tut'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -44,3 +44,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def html_tut(request):
+    return render(request, 'blog/html_tut.html')
